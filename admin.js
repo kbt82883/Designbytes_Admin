b8c87b9c996fbc25b8dd1dc752041c37f28a4eeb
kbt <kbt82883@gmail.com>
2024-03-09 17:20:24 +0900

    24/03/09

---
+++ admin.js
@@ -0,0 +1,26 @@
+// 로그인
+$('#login-form').submit(function (e) { 
+    $.ajax({
+        type: "post",
+        url: "url",
+        data: "data",
+        dataType: "dataType",
+        success: function (response) {
+            
+        }
+    });
+});
+
+// 영상 업로드
+$('#ubload').submit(function (e) { 
+    $.ajax({
+        type: "method",
+        url: "url",
+        data: "data",
+        dataType: "dataType",
+        success: function (response) {
+            
+        }
+    });
+    
+});
