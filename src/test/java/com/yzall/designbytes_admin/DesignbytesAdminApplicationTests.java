d1482c9815ef71f58592bf637edecbf6794f1856
HakSeong Lee <kth4241@gmail.com>
2024-03-09 18:00:31 +0900

    gitignore

---
+++ src/test/java/com/yzall/designbytes_admin/DesignbytesAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzall.designbytes_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignbytesAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
