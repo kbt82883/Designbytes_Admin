93062e097b6f8fc931a4fd0121ff56285f7adf52
kbt <kbt82883@gmail.com>
2024-03-29 15:45:38 +0900

    03/29 15:45

--- src/main/resources/static/script/admin.js
+++ src/main/resources/static/script/admin.js
@@ -1,5 +1,4 @@
 // 로그인
-<<<<<<< HEAD:admin.js
 $("#login-form").submit(function (e) {
   $.ajax({
     type: "post",
@@ -20,30 +19,3 @@ $("#ubload").submit(function (e) {
     success: function (response) {},
   });
 });
-=======
-$('#login-form').submit(function (e) {
-    $.ajax({
-        type: "post",
-        url: "url",
-        data: "data",
-        dataType: "dataType",
-        success: function (response) {
-
-        }
-    });
-});
-
-// 영상 업로드
-$('#ubload').submit(function (e) {
-    $.ajax({
-        type: "method",
-        url: "url",
-        data: "data",
-        dataType: "dataType",
-        success: function (response) {
-
-        }
-    });
-
-});
->>>>>>> d1482c9815ef71f58592bf637edecbf6794f1856:src/main/resources/static/script/admin.js
